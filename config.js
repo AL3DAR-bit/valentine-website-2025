218e9c3999bbf054279eb7a1c129db461cd1d4d4
AL3DAR-bit <al3dar2003@gmail.com>
2026-04-07 19:05:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "𝒔𝒉𝒓𝒊𝒚",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
